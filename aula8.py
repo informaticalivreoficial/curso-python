7de53fc24dde3662c33573a2d816b3e7dc89099f
renato-Aspire-E5-574 <renatomontanari@ymail.com>
2024-01-02 00:23:10 -0300

    aula 8 e 9

---
+++ aula8.py
@@ -0,0 +1,8 @@
+#nome = input('Qual seu nome?')
+
+#print(f'Seu nome é {nome=}')
+
+numero_1 = int(input('digite um numero: '))
+numero_2 = int(input('digite outro número: '))
+
+print(f'resultado: {numero_1 + numero_2}')
---
+++ aula9.py
@@ -0,0 +1,8 @@
+entrada = input('você quer entrar ou sair?')
+
+if entrada == 'sair':
+    print('você saiu do sistema!')
+elif entrada == 'entrada':
+    print('você entrou no sistema!')
+else:
+    print('você não entrou nem saiu!!!')
