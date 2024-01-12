a478e4f9bcc32bb79c05291af496f2ca3378edf2
renato-Aspire-E5-574 <renatomontanari@ymail.com>
2024-01-12 10:50:42 -0300

    aula 10

---
+++ aula10.py
@@ -0,0 +1,7 @@
+primeiro_valor = input('digite o primeiro valor:')
+segundo_valor = input('digite o segundo valor:')
+
+if primeiro_valor >= segundo_valor:
+    print(f'{primeiro_valor=} é maior ou igual ao {segundo_valor=}')
+else:
+    print(f'{segundo_valor=} é maior do que o {primeiro_valor=}')
